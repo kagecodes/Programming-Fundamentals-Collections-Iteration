13f6c7af2de981ea78e97ad0b5d770656b0b16b6
Karen <kjgamis@live.ca>
2018-03-21 15:15:47 -0400

    finish exercise 2

--- exercise.rb
+++ exercise.rb
@@ -6,7 +6,16 @@ artists = ["The 1975", "Pentatonix", "The Killers"]
 fav_colours_symbols = [:red, :black, :white]
 
 # Hashes
-words = {"dedicated": "devoted to a task or purpose", "encourage": "to give support, confidence or hope", "appearance": "the way something or someone looks"}
-movies = {"The Avengers": 2018, "The Lion King": 1996, "Frozen": 2013}
-countries = {"Canada": 33000000, "USA": 325000000, "Switzerland": 8000000}
-family = {"Kat": 22, "Karen": 21, "Ernest": 17}
+words = {dedicated: "devoted to a task or purpose", encourage: "to give support, confidence or hope", appearance: "the way something or someone looks"}
+movies = {The_Avengers: 2018, The_Lion_King: 1996, Frozen: 2013}
+countries = {Canada: 33000000, USA: 325000000, Switzerland: 8000000}
+family = {Kat: 22, Karen: 21, Ernest: 17}
+
+
+# Exercise 1
+
+puts coin_flip
+puts fav_colours[0]
+puts age.sort
+puts age << 0
+puts movies[:The_Avengers]
