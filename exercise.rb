87573732c35b00f7bfc7942b142c957fce83ce55
Karen <kjgamis@live.ca>
2018-03-21 15:28:50 -0400

    finish exercise 2

--- exercise.rb
+++ exercise.rb
@@ -8,17 +8,38 @@ fav_colours_symbols = [:red, :black, :white]
 # Hashes
 words = {dedicated: "devoted to a task or purpose", encourage: "to give support, confidence or hope", appearance: "the way something or someone looks"}
 movies = {The_Avengers: 2018, The_Lion_King: 1996, Frozen: 2013}
-countries = {Canada: 33000000, USA: 325000000, Switzerland: 8000000}
+cities = {Toronto: 2700000, NewYork: 8500000, Switzerland: 670000}
 family = {Kat: 22, Karen: 21, Ernest: 17}
+
 
 
 # Exercise 1
 
-puts coin_flip
-puts fav_colours[0]
-puts age.sort
-puts age << 0
-puts movies[:The_Avengers]
+
+# puts coin_flip
+
+# puts fav_colours[0]
+
+# puts age.sort
+
+# puts age << 0
+
+# puts movies[:The_Avengers]
 
 
- 
+
+# Exercise 2
+
+
+puts fav_colours[-1]
+
+cities[:SaltLakeCity] = "190000"
+puts cities
+
+puts coin_flip.reverse
+
+puts cities[:NewYork]
+
+artists.each do |name|
+  puts "I think #{name} is great."
+end
