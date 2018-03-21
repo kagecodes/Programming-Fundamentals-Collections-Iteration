40be7dc3738c4a0b30e9b1a8fe0a1a948a1b996c
Karen <kjgamis@live.ca>
2018-03-21 15:04:24 -0400

    finish exercise 0

---
+++ exercise.rb
@@ -0,0 +1,12 @@
+# Arrays
+fav_colours = ["red", "black", "white"]
+age = [17, 21, 22]
+coin_flip = ["heads", "heads", "tails", "heads", "tails"]
+artists = ["The 1975", "Pentatonix", "The Killers"]
+fav_colours_symbols = [:red, :black, :white]
+
+# Hashes
+words = {"dedicated": "devoted to a task or purpose", "encourage": "to give support, confidence or hope", "appearance": "the way something or someone looks"}
+movies = {"The Avengers": 2018, "The Lion King": 1996, "Frozen": 2013}
+countries = {"Canada": 33000000, "USA": 325000000, "Switzerland": 8000000}
+family = {"Kat": 22, "Karen": 21, "Ernest": 17}
