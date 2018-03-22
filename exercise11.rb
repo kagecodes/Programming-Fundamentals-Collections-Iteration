2fc081497b9e278e932b7718ce7dd4ed9a6f0494
Karen <kjgamis@live.ca>
2018-03-22 11:00:13 -0400

    fix order mistake

--- exercise11.rb
+++ exercise11.rb
@@ -3,12 +3,12 @@ numbers = (1..100)
 # puts numbers
 
 numbers.each do |integer|
-  if integer % 3 == 0
+  if integer % 3 == 0 && integer % 5 == 0
+    puts "BitMaker"
+  elsif integer % 3 == 0
     puts "Bit"
   elsif integer % 5 == 0
     puts "Maker"
-  elsif integer % 3 == 0 && integer % 5 == 0
-    puts "BitMaker"
   else
     puts integer
   end
