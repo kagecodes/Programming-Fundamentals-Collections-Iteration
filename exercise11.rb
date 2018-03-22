bc0c0de04122a06acf05c59167f57db31310ea0c
Karen <kjgamis@live.ca>
2018-03-22 08:15:39 -0400

    finish exercise 11

---
+++ exercise11.rb
@@ -0,0 +1,14 @@
+numbers = (1..100)
+
+# puts numbers
+
+numbers.each do |integer|
+  if integer % 3 == 0
+    puts "Bit"
+  elsif integer % 5 == 0
+  elsif integer % 3 == 0 && integer % 5 == 0
+    puts "BitMaker"
+  else
+    puts integer
+  end
+end
