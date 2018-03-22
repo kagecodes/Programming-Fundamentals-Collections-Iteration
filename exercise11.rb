a5ad44a48b3caac2adab3241e1155fd4b616691f
Karen <kjgamis@live.ca>
2018-03-22 10:49:57 -0400

    add missing puts Maker part

--- exercise11.rb
+++ exercise11.rb
@@ -6,6 +6,7 @@ numbers.each do |integer|
   if integer % 3 == 0
     puts "Bit"
   elsif integer % 5 == 0
+    puts "Maker"
   elsif integer % 3 == 0 && integer % 5 == 0
     puts "BitMaker"
   else
